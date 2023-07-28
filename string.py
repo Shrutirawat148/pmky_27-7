a7d63be6a35cdb14c2c8a7f86f0c7ce4d930044e
Shrutirawat148 <140155229+Shrutirawat148@users.noreply.github.com>
2023-07-28 14:13:47 +0530

    Create string.py
    these are the string function

---
+++ string.py
@@ -0,0 +1,37 @@
+a="hello world"
+print(a);
+a="hello world"
+print(a[:2])
+a="hello world"
+print(a[5]);
+a="hello world"
+print(a[2:]);
+a="hello world"
+print(a[2:5]);
+a="hello world"
+print(a[5:]+"rest");
+a="hello world"
+print(a[5:]+" "+"rest");
+str1 = "Hello, world!"
+str2 = "I love Python."
+str3 = "Hello, world!"
+
+# compare str1 and str2
+print(str1 == str2)
+
+# compare str1 and str3
+print(str1 == str3)
+greet = "Hello, "
+name = "Jack"
+
+# using + operator
+result = greet + name
+print(result)
+a="hello world"
+print(len(a));
+a="hello world"
+print('a' in a );
+a="hello world"
+print("wor" in a);
+a="hello world"
+print("wor" not in a);
